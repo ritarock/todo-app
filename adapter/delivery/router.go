008aa557527898e66607229e13eb85990d035251
ritarock <dvryz1204@gmail.com>
2024-08-03 16:02:11 +0900

    fix

--- adapter/delivery/router.go
+++ adapter/delivery/router.go
@@ -1,9 +1,24 @@
 package delivery
 
-import "github.com/labstack/echo/v4"
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
 
 func NewRouter(taskHandler *taskHandler) *echo.Echo {
 	e := echo.New()
+
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"http://localhost:5173"},
+		AllowHeaders: []string{
+			echo.HeaderOrigin,
+			echo.HeaderContentType,
+			echo.HeaderAccept,
+			echo.HeaderAuthorization,
+		},
+	}))
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
 
 	e.POST("/tasks", taskHandler.Create)
 	e.GET("/tasks", taskHandler.GetAll)
